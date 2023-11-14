d8de3cbb5262ea9372d248f9e2be472df75be2a2
jesikabar1 <jesicabbarrionuevo@gmail.com>
2023-11-14 19:48:11 -0300

    color y fuente

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnMenu = document.getElementById('btn-menu');
+let nav = document.getElementById('menu');
+
+btnMenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+    document.get
+});
